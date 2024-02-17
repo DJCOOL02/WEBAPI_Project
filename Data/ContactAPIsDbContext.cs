a7210de94ff503215448ebb3f13ff31d699520c8
Divyanshu <divyanshujasoria@gmail.com>
2024-02-17 16:20:52 +0530

    Add project files.

---
+++ Data/ContactAPIsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEBAPI.Models;
+
+namespace WEBAPI.Data
+{
+    public class ContactAPIsDbContext: DbContext
+    {
+        public ContactAPIsDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Contacts> Contacts { get; set; }
+    }
+}
